01f08731aeefc3052158fb809774fe9b5a6850ec
yunhyosang <yunhyosang@naver.com>
2015-01-12 23:34:19 +0900

    수정

--- js/common.js
+++ js/common.js
@@ -2,11 +2,15 @@ $(document).ready(function(){
 	
 	lnbTitle();
 	moveElm();
+//	lnb_2Depth();
 
 	$(window).resize(function(){
 	// resize 이벤트용
+		var winW = $(window).width(); 
 		lnbTitle();
 		moveElm();
+//		lnb_2Depth();
+
 
 	});
 
@@ -16,15 +20,17 @@ $(document).ready(function(){
 
 	/* sub layout */
 	var lnb_1dp = $(".menu_list>li");
+
 	lnb_1dp.click(function(){
-		$(".menu_list>li").removeClass("on")
-		$(this).addClass('on')
+		$(".menu_list>li").removeClass("on");
+		$(this).addClass('on');
+
 	});
 
 	/* 모바일 메뉴아이콘 클릭 */
 	var menu_btn = $('.menu_btn')
 		, gnb = $(".gnb")
-		dimmed = $("<div class='dimmed'></div>");
+		, dimmed = $("<div class='dimmed'></div>");
 
 	menu_btn.click(function(){
 		var headerH = $("#header").height()
@@ -38,7 +44,7 @@ $(document).ready(function(){
 			dimmed.css({"top":dimmedTop});
 		}else{
 			gnb.slideUp();
-			dimmed.hide()
+			dimmed.hide();
 		}
 	});
 
@@ -49,30 +55,43 @@ $(document).ready(function(){
 		var  lnbTitle = $(".lnb_tit"); // lnb 메뉴 타이틀
 			if((winW<940) && ($("#container").has("#aside"))){
 					lnbTitle.insertAfter(".logo").addClass("mobile_tit");
-				console.log('a')
 			}else if((winW>940) && ($("#container").has("#aside"))){
 					lnbTitle.insertBefore(".menu_list").removeClass("mobile_tit");
 			}
 	}
 
 	function moveElm(){
-		// 상단 로그인, 가입, lnb 의 2depth 위치이동
+		// 상단 로그인, 가입, lnb 의 2depth 위치이동 및 푸터 내 컨텐츠 위치변경
 		var login_join = $(".login, .join")
-			, sub_menu = $("<div class='sub_menu'></div>");
+			, foot_utils = $(".foot_utils");
 		var winW = $(window).width(); 
 
 		if(940>winW){
 			login_join.insertBefore('.provision');
-
+			foot_utils.insertBefore('address');
+			$("#footer span:nth-child(2)").css({"display":"none"});	
 		}else{
 			login_join.insertBefore('.util .contact');
+			$("#footer span:nth-child(2)").css({"display":"inline-block"});
 		}
 		
 	}
-
-	function lnb_2Depth(){
-
-	}
-
 });
 
+
+//	function lnb_2Depth(){
+//		var winW = $(window).width();
+//		if(940>winW){
+//			var menu_list = $(".menu_list")
+//				, sub_menu = $(".sub_menu")
+//				, menu_list_1dp = $(".menu_list>li.on")
+//				, depth2 = $(".menu_list>li ul")
+//				, cloneD2 = $(".menu_list>li ul").clone();
+//				
+//				menu_list.after("<div class='sub_menu'></div>");
+//				depth2.hide();
+//				$('.sub_menu').append(cloneD2)
+//		}else{
+//			$(".menu_list>li.on>ul").show()
+//		}
+//	}
